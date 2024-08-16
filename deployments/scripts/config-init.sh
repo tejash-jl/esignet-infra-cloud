2d0f3f1e4de54cae0332d4d88659eef62665a8d0
Tejash JL <tejash.jl1020@gmail.com>
2024-08-16 15:18:35 +0530

    Add scripts to configure keycloak
    (cherry picked from commit 0e4b795f599c9b38b879cc2943d6a16bc16f2e1d)
    Signed-off-by: Tejash JL <tejash.jl1020@gmail.com>

--- deployments/scripts/config-init.sh
+++ deployments/scripts/config-init.sh
@@ -4,15 +4,16 @@ apt-get install jq gettext -y
 LOCAL_ESIGNET_PROPERTIES=deployments/configs/esignet.properties
 UPDATED_ESIGNET_PROPERTIES=esignet-local.properties
 
-export ESIGNET_HOST=https://$1/esignet/
+export ESIGNET_HOST=$1
 export API_INTERNAL=https://$1/esignet/
-export KEYCLOAK_URL=https://$1/auth/
+export KEYCLOAK_URL=$1
 export SOFTHSM_PIN=$(kubectl get secrets softhsm -n esignet -o jsonpath={.data.security-pin} | base64 --decode)
 export KAFKA_URL=kafka-cluster-kafka-bootstrap.kafka:9092
 export DB_HOST=$(gcloud sql instances describe $2 --format=json  | jq -r ".ipAddresses[0].ipAddress")
 export DB_PORT=5432
 export DB_USERNAME=postgres
 export DB_PASSWORD=$(gcloud secrets versions access latest --secret $3)
+export REDIS_HOST=$(gcloud redis instances describe esignet-dev-redis --region asia-south1 --format=json | jq -r ".host")
 
 envsubst < $LOCAL_ESIGNET_PROPERTIES  > $UPDATED_ESIGNET_PROPERTIES
 
